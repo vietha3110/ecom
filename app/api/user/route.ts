17e85c81b72fa17bb1e9de450a9359a73e00fea2
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-09-27 22:56:59 -0700

    added create user info route.

---
+++ app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { db } from '@/lib/db'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const newUser = await db.user.create({
+            data: {
+                name: body.name,
+                email: body.email,
+                address: body.address,
+                phone: body.phone,
+            }
+        });
+        return NextResponse.json(newUser, { status: 201 });
+    } catch (e) {
+        console.log(e);
+        return NextResponse.json({message: 'There is an error. Please try again'}, {status: 500})
+    }
+}
